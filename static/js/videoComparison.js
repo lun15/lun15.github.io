982d35e0a73c410e859000f358e1a911d0c0f2ba
Lun <hcl10616@gmail.com>
2024-01-02 18:37:10 +0800

    video comparison

--- static/js/videoComparison.js
+++ static/js/videoComparison.js
@@ -4,16 +4,18 @@ window.addEventListener("load",init,false);
 
 function init(){
     function trackLocation(e) {
-        var rect = videoContainer.getBoundingClientRect(),
-        position = ((e.pageX - rect.left) / videoContainer.offsetWidth)*100;
-        clippedVideo.style.left =  position+"%";
-
+        var rect = videoContainer.getBoundingClientRect();
+        var position = ((e.pageX - rect.left) / videoContainer.offsetWidth)*100;
+        if (position <= 100) { 
+          videoClipper.style.width = position+"%";
+          clippedVideo.style.width = ((100/position)*100)+"%";
+          clippedVideo.style.zIndex = 3;
+        }
     }
-    var videoContainer = document.getElementById("video-compare-container");
-    var videoClipper = document.getElementById("video-clipper");
-    var clippedVideo = videoClipper.getElementsByTagName("video")[0];
-    
-    videoContainer.addEventListener("mousemove", trackLocation, false); 
+    var videoContainer = document.getElementById("video-compare-container"),
+    videoClipper = document.getElementById("video-clipper"),
+    clippedVideo = videoClipper.getElementsByTagName("video")[0];
+    videoContainer.addEventListener( "mousemove", trackLocation, false); 
     videoContainer.addEventListener("touchstart",trackLocation,false);
     videoContainer.addEventListener("touchmove",trackLocation,false);
       
