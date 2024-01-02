2eb2279d7ff9bd24ba6b522995c0e5c634d4f336
Lun <hcl10616@gmail.com>
2024-01-03 02:38:52 +0800

    preprocessing

--- static/js/videoComparison.js
+++ static/js/videoComparison.js
@@ -3,6 +3,8 @@ window.addEventListener("load",init,false);
 
 
 function init(){
+
+    /* video comparison bar*/
     function trackLocation(e) {
         var rect = videoContainer.getBoundingClientRect();
         var position = ((e.pageX - rect.left) / videoContainer.offsetWidth)*100;
@@ -18,5 +20,28 @@ function init(){
     videoContainer.addEventListener( "mousemove", trackLocation, false); 
     videoContainer.addEventListener("touchstart",trackLocation,false);
     videoContainer.addEventListener("touchmove",trackLocation,false);
-      
+
+
+    /* enlarge image on hover*/
+    var images = document.getElementsByClassName("normalize-image");
+
+    Array.prototype.forEach.call(images,image => {    
+        image.addEventListener("click", (e)=>{            
+            var container = document.getElementById("enlarge-container")
+            var label = container.childNodes[0];
+            var img = container.childNodes[1];
+            img.src = e.target.src;
+            label.innerHTML = "result";
+            container.hidden = false;
+
+        }, false);        
+
+        image.addEventListener("mouseleave", (e)=>{
+            var container = document.getElementById("enlarge-container")
+            var label = container.getAttribute("label");
+            var img = container.getAttribute("img");
+            container.hidden = true;
+
+        }, false);  
+    });      
 }
