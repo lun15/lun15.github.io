59a23903c5cf2145dc234e9655adbb7b62cf9310
Lun <hcl10616@gmail.com>
2024-01-03 09:52:55 +0800

    attention video comparison

--- static/js/videoComparison.js
+++ static/js/videoComparison.js
@@ -3,22 +3,34 @@ window.addEventListener("load",init,false);
 
 function init(){
 
-    /* video comparison bar*/
-    function trackLocation(e) {
-        var rect = videoContainer.getBoundingClientRect();
-        var position = ((e.pageX - rect.left) / videoContainer.offsetWidth)*100;
-        if (position <= 100) { 
-          videoClipper.style.width = position+"%";
-          clippedVideo.style.width = ((100/position)*100)+"%";
-          clippedVideo.style.zIndex = 3;
-        }
-    }
-    var videoContainer = document.getElementById("video-compare-container"),
-    videoClipper = document.getElementById("video-clipper"),
-    clippedVideo = videoClipper.getElementsByTagName("video")[0];
-    videoContainer.addEventListener( "mousemove", trackLocation, false); 
-    videoContainer.addEventListener("touchstart",trackLocation,false);
-    videoContainer.addEventListener("touchmove",trackLocation,false);
+    /* video comparison bar*/    
+    var videoContainers = document.getElementsByClassName("video-compare-container");
+    Array.prototype.forEach.call(videoContainers,videoContainer=>{        
+
+        var videoClippers = document.getElementsByClassName("video-clipper");
+        Array.prototype.forEach.call(videoClippers, videoClipper => {
+            function trackLocation(e) {
+                var rect = videoContainer.getBoundingClientRect();
+                var position = ((e.pageX - rect.left) / videoContainer.offsetWidth)*100;
+                if (position <= 100) { 
+                  videoClipper.style.width = position+"%";
+                //   video
+                  clippedVideo.style.width = ((100/position)*100)+"%";
+                  clippedVideo.style.zIndex = 3;
+                //   image
+                  clippedImage.style.width = ((100/position)*100)+"%";
+                  clippedImage.style.zIndex = 3;
+                }
+            }
+            var clippedVideo = videoClipper.getElementsByTagName("video")[0];   
+            var clippedImage = videoClipper.getElementsByTagName("img")[0]; 
+            videoContainer.addEventListener( "mousemove", trackLocation, false); 
+            videoContainer.addEventListener("touchstart",trackLocation,false);
+            videoContainer.addEventListener("touchmove",trackLocation,false);
+        })
+        
+    })
+    
 
 
     /* enlarge image on hover*/
